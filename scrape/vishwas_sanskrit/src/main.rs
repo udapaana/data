a197dff877a2c1e8de9edd91844955d41fff7f8d
śrīnivāsa kaśyap munukutla <skmnktl@gmail.com>
2024-05-30 17:15:02 -0500

    fixed name

--- scrape/vishwas_sanskrit/src/main.rs
+++ scrape/vishwas_sanskrit/src/main.rs
@@ -1,34 +1,49 @@
 use reqwest::blocking::get;
 use serde_json::json;
+use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
 use std::io::Write;
 
-fn get_urls() -> Vec<String>{
-
-    let template: String = "https://raw.githubusercontent.com/udapaana/raw_etexts/master/vedaH/yajur/taittirIya/mUlam/saMhitA".to_string();
-
-    let stubs: Vec<(i32, i32)> = vec![
-        (1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7),
-        (2,1),(2,2),(2,3),(2,4),(2,5),(2,6),
-    
-    ];
+fn get_samhita_urls() -> Vec<String> {
+    let samhita: String = "https://raw.githubusercontent.com/udapaana/raw_etexts/master/vedaH/yajur/taittirIya/mUlam/saMhitA".to_string();
+    // Each (k,p) value represents the number of p prasnas
+    // for the corresponding kanda starting with 1 and including the value listed.
+    let stubs: Vec<(i32, i32)> = vec![(1, 7), (2, 6), (3, 5), (4, 7), (5, 7), (6, 6), (7, 5)];
     let mut urls: Vec<String> = vec![];
-    for &(k,p) in stubs.iter(){
-        urls.push(
-           template.clone() + "/" + k.to_string().as_str() + "/" + p.to_string().as_str() + ".md" 
-        );
+    for &(k, bound) in stubs.iter() {
+        for p in 1..bound + 1 {
+            urls.push(
+                samhita.clone()
+                    + "/"
+                    + k.to_string().as_str()
+                    + "/"
+                    + p.to_string().as_str()
+                    + ".md",
+            );
+        }
     }
-    return urls
+    return urls;
 }
-        
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let urls = get_urls();
+    let urls = get_samhita_urls();
     // Iterate over URLs
-    for (file_index, &ref url) in urls.iter().enumerate() {
-        println!("{url}");
-        let kanda = url.split('/').nth(11).unwrap_or("-1").parse::<i32>().unwrap();
-        let prasna = url.split('/').last().unwrap_or("-1.md").trim_end_matches(".md").parse::<i32>().unwrap();
+    for &ref url in urls.iter() {
+        //println!("{url}");
+        let kanda = url
+            .split('/')
+            .nth(11)
+            .unwrap_or("-1")
+            .parse::<i32>()
+            .unwrap();
+        let prasna = url
+            .split('/')
+            .last()
+            .unwrap_or("-1.md")
+            .trim_end_matches(".md")
+            .parse::<i32>()
+            .unwrap();
 
         // Fetch text from URL
         let text = get(url)?.text()?;
@@ -36,25 +51,26 @@ fn main() -> Result<(), Box<dyn Error>> {
         // Parse verses
         let verses = parse_verses(&text);
 
-        let mut parsed = Vec::new();
+        let mut parsed = HashMap::new();
         // Output each verse as JSON
-        let mut index = 1;
+        let mut anuvaka = 1;
         for verse in verses.iter() {
             let stripped = strip_index(verse);
-            if !stripped.is_empty(){
+            if !stripped.is_empty() {
                 let json_output = json!({
                     "kanda": kanda,
                     "prasna": prasna,
-                    "anuvaka": index,
+                    "anuvaka": anuvaka,
                     "deva": strip_index(verse),
                 });
-                parsed.push(json_output);
-                index+=1;
+                parsed.insert(format!("{}.{}.{}", kanda, prasna, anuvaka), json_output);
+                anuvaka += 1;
             }
         }
+        println!("PROCESSED {}.{}.{}", kanda, prasna, anuvaka);
 
         // Write JSON to file
-        let mut file = File::create(format!("outputs/{}.json", file_index))?;
+        let mut file = File::create(format!("samhita/{kanda}.{prasna}.json"))?;
         file.write_all(serde_json::to_string_pretty(&parsed)?.as_bytes())?;
     }
 
@@ -71,4 +87,3 @@ fn parse_verses(text: &str) -> Vec<String> {
     // Assuming each verse is separated by a newline
     text.lines().map(|line| line.trim().to_string()).collect()
 }
-
